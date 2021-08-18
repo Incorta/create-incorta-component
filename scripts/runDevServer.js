a494be228e138be26d3d9cb86ab7214d8a4d96e9
Shehab Elhariry <cheha6@gmail.com>
2021-08-18 18:46:31 +0200

    added socket io and chokidar

--- scripts/runDevServer.js
+++ scripts/runDevServer.js
@@ -3,27 +3,56 @@ const { join } = require("path");
 const express = require("express");
 const chalk = require("chalk");
 const { shouldUseYarn: shouldUseYarnCheck } = require("../utils/has-yarn");
+const socket = require("socket.io");
+const cors = require("cors");
+const chokidar = require("chokidar");
 
 const runDevServer = async () => {
   try {
     console.log(chalk.gray("Starting dev server"));
     const currentProcessDir = process.cwd();
     const distPath = join(currentProcessDir, "dist");
-    const app = express();
-    app.listen(8000, function () {
-      app.use(express.static(distPath));
-      console.log(
-        chalk.gray(`
-                Dev server is running on:`)
-      );
-      console.log(
-        chalk.cyan(`
-                http://localhost:8000
-            `)
-      );
-    });
-    const shouldUseYarn = shouldUseYarnCheck(currentProcessDir);
 
+    // App setup
+    const PORT = 8000;
+    const app = express();
+
+    // TODO: Remove
+    var corsOptions = {
+      origin: "*",
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    };
+
+    app.use(cors(corsOptions));
+    const server = app.listen(PORT, function () {
+      console.log(`Listening on port ${PORT}`);
+      console.log(`http://localhost:${PORT}`);
+    });
+
+    app.use(express.static(distPath));
+
+    const io = socket(server);
+
+    io.on("connection", function (socket) {
+      const files = [
+        join(distPath, "bundle.css"),
+        join(distPath, "bundle.modern.js"),
+      ];
+
+      const watcher = chokidar.watch(files, {
+        persistent: true,
+        interval: 1000,
+      });
+
+      watcher.on("change", (path) => {
+        io.emit("update", { message: "file updated" + `(${path})` });
+        console.log("files updated");
+      });
+
+      console.log("Made socket connection");
+    });
+
+    const shouldUseYarn = shouldUseYarnCheck(currentProcessDir);
     if (shouldUseYarn) {
       await execa("yarn", ["start"]);
     } else {
