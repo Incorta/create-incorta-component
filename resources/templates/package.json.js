222f0d3e59477fa089fd99da9296f4030db71ba1
ahmedriyad <ahmed.riyad@incorta.com>
2021-09-08 12:59:48 +0200

    update package version

--- resources/templates/package.json.js
+++ resources/templates/package.json.js
@@ -68,7 +68,7 @@ module.exports = opts => {
     },
     files: ['dist'],
     dependencies: {
-      '@incorta-org/visual-sdk': '^0.1.2-alpha.4'
+      '@incorta-org/visual-sdk': '^0.1.2-alpha.6'
     }
   };
 };
