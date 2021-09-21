1fd184c4163ace8ea8486271b598d975d70d067f
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-09-21 17:09:30 +0200

    [3290] add new command

--- scripts/create-incorta-visual.js
+++ scripts/create-incorta-visual.js
@@ -1,36 +1,37 @@
-"use strict";
+'use strict';
 
-const { Command } = require("commander");
+const { Command } = require('commander');
 
-const runInit = require("./init/index");
-const runPackage = require("./package/index");
-const runStart = require("./start/index");
+const runInit = require('./init/index');
+const runPackage = require('./package/index');
+const runStart = require('./start/index');
 
-const checkBeforeInit = require("../utils/check-requirements");
+const checkBeforeInit = require('../utils/check-requirements');
 
-const createIncortaVisual = new Command("create-incorta-visual");
+const createIncortaVisual = new Command('create-incorta-visual');
 
-const packageJson = require("../package.json");
+const packageJson = require('../package.json');
 
 createIncortaVisual
-  .arguments("[directory]")
+  .command('new')
+  .description('Create a new Incorta visual')
+  .arguments('[directory]')
   .version(packageJson.version)
-  .description("Create a new Incorta visual")
-  .action((directory) => {
+  .action(directory => {
     checkBeforeInit();
     runInit(directory, createIncortaVisual);
   });
 
 createIncortaVisual
-  .command("start")
-  .description("Start development server")
+  .command('start')
+  .description('Start development server')
   .action(() => {
     runStart();
   });
 
 createIncortaVisual
-  .command("package")
-  .description("Build visualization and generate bundle.inc file")
+  .command('package')
+  .description('Build visualization and generate bundle.inc file')
   .action(() => {
     runPackage();
   });
