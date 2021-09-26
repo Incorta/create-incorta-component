76c7f75e3d603d501c75c7136fdc64dc6ec238e6
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-09-26 14:44:31 +0200

    change props type to ComponentsProps

--- resources/templates/index.tsx.js
+++ resources/templates/index.tsx.js
@@ -3,10 +3,10 @@ const { camelCase, startCase } = require('lodash');
 const visualizationIndexGenerator = ({ directory }) => {
   const pascalCaseName = startCase(camelCase(directory)).replace(/ /g, '');
   return `import React from 'react';
-import { VisualProps } from '@incorta-org/visual-sdk';
+import { ComponentsProps } from '@incorta-org/visual-sdk';
 import './styles.less';
 
-const ${pascalCaseName} = (props: VisualProps) => {
+const ${pascalCaseName} = (props: ComponentsProps) => {
   console.log(props);
   return (
     <div className="test">
