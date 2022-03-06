740b3d749336eda07f0c48ea9172a2e272bbc4c9
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2022-03-06 19:52:19 +0200

    update index.tsx sample

--- resources/templates/index.tsx.js
+++ resources/templates/index.tsx.js
@@ -3,16 +3,29 @@ const { camelCase, startCase } = require('lodash');
 const componentIndexGenerator = ({ directory }) => {
   const pascalCaseName = startCase(camelCase(directory)).replace(/ /g, '');
   return `import React from 'react';
-import { useContext } from '@incorta-org/component-sdk';
+import {
+  useContext,
+  LoadingOverlay,
+  ErrorOverlay,
+  usePrompts,
+  useQuery
+} from '@incorta-org/component-sdk';
 import './styles.less';
 
 const ${pascalCaseName} = () => {
-  const context = useContext();
-  console.log(context);
+  const { prompts } = usePrompts();
+  const { data, context, isLoading, isError, error } = useQuery(useContext(), prompts);
+
+  console.log({ context, data });
+
   return (
-    <div className="test">
-      <h1>Hello incorta Component</h1>
-    </div>
+    <ErrorOverlay isError={isError} error={error}>
+      <LoadingOverlay isLoading={isLoading} data={data}>
+        <div className="test">
+          <h1>Hello incorta Component</h1>
+        </div>
+      </LoadingOverlay>
+    </ErrorOverlay>
   );
 };
 
