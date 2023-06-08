7b8c57b4a21d7a63b7741dc7341f1771fdc51afd
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2023-06-08 13:14:32 +0300

    use node spawn with test command

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -1,8 +1,10 @@
-const shelljs = require('shelljs');
+const { spawn } = require('child_process');
 
 async function runTest() {
   try {
-    shelljs.exec(`npx vitest`);
+    // use spawn to run an interactive command
+    const shell = spawn('npx vitest', [], { stdio: 'inherit', shell: true });
+    shell.on('close', code => console.log('[shell] terminated :', code));
   } catch (e) {
     console.log(e);
   }
